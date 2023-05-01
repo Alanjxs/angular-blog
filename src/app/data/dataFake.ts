9394564861e628b0aee5895f33b47044345784ea
Alan JXS <alanjxs@gmail.com>
2023-05-01 09:26:37 -0300

    Criação do meu blog para divulgação das minhas aulas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Como criar link para enviar mensagem para o whatsapp",
+    "description": "Utilizando o seguinte código no seu link, <a href='https://api.whatsapp.com/send?phone=numerocompletodocelular&text=Aqui você coloca o texto''>Enviar Mensagem</a>, você conseguirá usar API do Whatsapp para enviar uma mensagem personalizada:",
+    "photoCover":"https://alandomingues.com.br/wp-content/uploads/2018/01/mensagem-personalizada-whatsapp.png"
+  },
+  {
+    "id":"2",
+    "title": "Como instalar Windows 10 e Ubuntu",
+    "description": "Acesse o link https://www.youtube.com/watch?v=k9EC2SoNmNk e acompanhe o passo a passo",
+    "photoCover":"https://i.ytimg.com/vi/k9EC2SoNmNk/hqdefault.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Aula de Administração de Redes - Projeto, PMBOK e PDCA",
+    "description": "Acesse o link https://youtu.be/usWBgnoWVpA e acompanhe o passo a passo",
+    "photoCover":"https://i.ytimg.com/vi/usWBgnoWVpA/hqdefault.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Aula de Gestão de Tempo",
+    "description": "Acesse o link https://youtu.be/XkYqRqzdoIM e acompanhe o passo a passo",
+    "photoCover":"https://i.ytimg.com/vi/XkYqRqzdoIM/hqdefault.jpg"
+  }
+]
